3f9a58486689ac1d5fc90da72f9fb560ad29a560
FAST PAY <mstaysab96@gmail.com>
2024-10-16 07:18:39 +0600

    Update script.js

--- repayyx.payxfast.com/script.js
+++ repayyx.payxfast.com/script.js
@@ -1,5 +1,5 @@
 function copyAmount() {
-    const amount = '₹ 4000';
+    const amount = '₹ 4700';
     navigator.clipboard.writeText(amount).then(() => {
         showPopup('copyModal', 'Amount copied to clipboard!');
     }).catch(err => {
